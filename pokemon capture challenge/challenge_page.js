383cf44c27bbfa28b39a2b4b6ac5d4601098a2f0
Atharv <atharvworkacc@gmail.com>
2025-11-08 12:37:10 +0530

    tried to generate random numbers to pass in pokemons and their stats to challenger, also tried to fetch array of stats of the pokemon listed.

--- pokemon capture challenge/challenge_page.js
+++ pokemon capture challenge/challenge_page.js
@@ -1,27 +1,67 @@
 const Pokecoins = document.getElementById('coins');
+const poke_name = document.getElementById('Pokemon_name')
 
 let coins = 1000;
+let randint = 0;
 Pokecoins.textContent = coins;
 
- function fetchPokemon() {
-      const name = document.getElementById('pokemon-input').value.trim().toLowerCase();
-      const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+function randgen(){
+    let randomNumber = Math.floor(Math.random() * 11)
+    randint = randomNumber
+    return randomNumber
+}
+function fetchPokemon(){
+   if(set === true){
+    const name = document.getElementById('pokemon-input').value.trim().toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
       
-      fetch(url)
+    fetch(url)
         .then(response => {
           if (!response.ok) throw new Error('Pokémon not found');
           return response.json();
         })
     
         .then(data => {
-          document.getElementById('result').innerHTML = `
-            <h2>${data.name}</h2>
-            <img src="${data.sprites.front_default}" alt="${data.name}">
-            `;
+            const statsArrayOfObjects = data.stats;
+            const statsArray = statsArrayOfObjects.map(statObject => statObject.base_stat);
+            document.getElementById('result').innerHTML = `
+            <h2>${data.name} Stats</h2>
+            <p>${statsArray.join(', ')}</p>
+        `;
         })
         .catch(error => {
           document.getElementById('result').innerHTML = `<span style="color:red;">${error.message}</span>`;
-        });
+        });} 
+}
+
+function fetchPokemon() {
+    if(set === true){
+    const name = document.getElementById('pokemon-input').value.trim().toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+      
+    fetch(url)
+        .then(response => {
+          if (!response.ok) throw new Error('Pokémon not found');
+          return response.json();
+        })
+    
+        .then(data => {
+            const statsArrayOfObjects = data.stats;
+            const statsArray = statsArrayOfObjects.map(statObject => statObject.base_stat);
+            document.getElementById('result').innerHTML = `
+            <h2>${data.name} Stats</h2>
+            <p>${statsArray.join(', ')}</p>
+        `;
+        })
+        .catch(error => {
+          document.getElementById('result').innerHTML = `<span style="color:red;">${error.message}</span>`;
+        });}
+}
+
+while (coins >0){
+    new_poke_chalenge()
+    set === true
+    setTimeout(randgen(), 2000)
 }
     
 
