69b4b200e3a233b399b20fac946a36d64f2cfef3
Atharv <atharvworkacc@gmail.com>
2025-11-09 12:06:44 +0530

    tried to improve random function and added var.

--- pokemon capture challenge/challenge_page.js
+++ pokemon capture challenge/challenge_page.js
@@ -1,19 +1,26 @@
 const Pokecoins = document.getElementById('coins');
-const poke_name = document.getElementById('Pokemon_name')
+const poke_name = document.getElementById('Pokemon_name');
+const type_of_stat = document.getElementById('type_of_stat');
+const stat_number = document.getElementById('sts');
+const resultDiv = document.getElementById('result');
+const pokemonInput = document.getElementById('pokemon-input');
 
+const stat_names = ["HP", "Attack", "Defense", "Special-Attack", "Special-Defense", "Speed"];
 let coins = 1000;
 let randint = 0;
+let challengePokemon;
+let challengeStatIndex;
 Pokecoins.textContent = coins;
 
-function randgen(){
-    let randomNumber = Math.floor(Math.random() * 11)
-    randint = randomNumber
-    return randomNumber
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function fetchPokemon(){
+function new_poke_chalenge(){
    if(set === true){
-    const name = document.getElementById('pokemon-input').value.trim().toLowerCase();
-    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    const poke = getRandomInt(1, 898);
+    const url = `https://pokeapi.co/api/v2/pokemon/${poke}`;
       
     fetch(url)
         .then(response => {
@@ -23,10 +30,13 @@ function fetchPokemon(){
     
         .then(data => {
             const statsArrayOfObjects = data.stats;
+            let fname = url.name
+            document.getElementById('Pokemon_name').innerHTML = `
+            <p>${fname}</p>
+        `;
             const statsArray = statsArrayOfObjects.map(statObject => statObject.base_stat);
-            document.getElementById('result').innerHTML = `
-            <h2>${data.name} Stats</h2>
-            <p>${statsArray.join(', ')}</p>
+            document.getElementById('sts').innerHTML = `
+            <p>${statsArray[stat]}</p>
         `;
         })
         .catch(error => {
@@ -58,10 +68,12 @@ function fetchPokemon() {
         });}
 }
 
-while (coins >0){
+for(let i =0; i<3; i++){
+    set = true
+    setTimeout(getRandomInt(0, 5), 2000)
     new_poke_chalenge()
-    set === true
-    setTimeout(randgen(), 2000)
+    let stat = getRandomInt(0,6)
+    let poke = getRandomInt(0,898)
 }
     
 
